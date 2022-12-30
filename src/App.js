9b857db1c33dff7c7718f8c1cc154a123047be58
Daniel <dselasea@gmail.com>
2022-12-30 02:35:41 +0000

    Functional component and adding styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
-
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer';
 function App() {
-
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Dave']
-    const int = Math.floor(Math.random() * 3)
-    return names[int]
-  }
-
   return (
     <div className="App">
-      <h1>Hello {handleNameChange()}!</h1>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
